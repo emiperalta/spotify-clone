4d1f5b5fe4c0876e5ba3955ceed68bb42d6d6be7
Emiliano Peralta <emilianop96@hotmail.com>
2021-07-21 11:46:18 -0300

    add spotify login service

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,26 @@
 require('dotenv/config');
 const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const credentials = require('../utils/credentials');
 
 const app = express();
 
 const { PORT } = process.env;
 
-app.get('/', (req, res) => res.send('hello world!'));
+app.post('/login', (req, res) => {
+  const { code } = req.body;
+  const spotifyApi = new SpotifyWebApi(credentials);
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(err => res.status(400).json(err));
+});
 
 app.listen(PORT, () => console.log('sever running at port', PORT));
