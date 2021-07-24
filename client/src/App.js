683e301629d7982a5d587265b28b799803d426b9
Emiliano Peralta <emilianop96@hotmail.com>
2021-07-24 15:49:34 -0300

    use dashboard in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import Login from 'components/Login';
+import Dashboard from 'components/Dashboard';
+
+const code = new URLSearchParams(window.location.search).get('code');
 
 export default function App() {
-  return <Login />;
+  return <div>{code ? <Dashboard code={code} /> : <Login />}</div>;
 }
