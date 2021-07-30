4106900084f65a6bbbd723ac4093ba45fd994856
Emiliano <emilianop96@hotmail.com>
2021-07-29 21:42:02 -0300

    use spotify web api to search song

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,6 +1,60 @@
+import { useEffect, useState } from 'react';
+import { Container, Form } from 'react-bootstrap';
+import SpotifyWebApi from 'spotify-web-api-node';
+
 import useAuth from 'hooks/useAuth';
 
+import { CLIENT_ID } from 'utils/auth';
+
+const spotifyApi = new SpotifyWebApi({ CLIENT_ID });
+
 export default function Dashboard({ code }) {
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
   const accessToken = useAuth({ code });
-  return <div>{accessToken}</div>;
+
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    let cancel = false;
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
+    spotifyApi.searchTracks(search).then(res => {
+      if (cancel) return;
+      setSearchResults(
+        res.body.tracks.items.map(track => {
+          const smallestAlbumImage = track.album.images.reduce((smallest, image) => {
+            if (image.height < smallest.height) return image;
+            return smallest;
+          }, track.album.images[0]);
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumImage,
+          };
+        })
+      );
+    });
+    return () => (cancel = true);
+  }, [search, accessToken]);
+
+  return (
+    <Container className='d-flex flex-column py-2' style={{ height: '100vh' }}>
+      <Form.Control
+        onChange={e => setSearch(e.target.value)}
+        placeholder='Search songs...'
+        type='search'
+        value={search}
+      />
+      <div className='flex-grow-1 my-2' style={{ overflowY: 'auto' }}>
+        Songs
+      </div>
+      <div>Bottom</div>
+    </Container>
+  );
 }
