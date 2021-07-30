3b514a0f0ae858e33d2e37794057d7b3d98c722c
Emiliano <emilianop96@hotmail.com>
2021-07-29 21:39:49 -0300

    add client id

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -2,3 +2,5 @@ const { REACT_APP_CLIENT_ID } = process.env;
 const { REACT_APP_URI } = process.env;
 
 export const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${REACT_APP_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+
+export const CLIENT_ID = REACT_APP_CLIENT_ID;
