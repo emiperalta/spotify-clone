a46ecd164dea331cbc03b66cfdcab477b80f932d
Emiliano <emilianop96@hotmail.com>
2021-07-30 22:43:21 -0300

    add styles and handle play

--- client/src/components/Track/index.js
+++ client/src/components/Track/index.js
@@ -1,6 +1,13 @@
-export default function Track({ track }) {
+import './Track.css';
+
+export default function Track({ chooseTrack, track }) {
+  const handlePlay = () => chooseTrack(track);
+
   return (
-    <div className='d-flex align-items-center m-2'>
+    <div
+      className='d-flex align-items-center p-2 track-container'
+      onClick={handlePlay}
+    >
       <img alt='album url' src={track.albumUrl.url} />
       <section className='px-2'>
         <div className='text-white'>{track.title}</div>
